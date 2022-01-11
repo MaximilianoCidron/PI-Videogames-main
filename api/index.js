dd7a7a47e1f9a8adc5184c236d89940db9468187
Maximiliano Cidron <maximilianocidron@gmail.com>
2022-01-11 17:00:03 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -17,12 +17,45 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+const server = require("./src/app.js");
+const axios = require("axios");
+const { conn, Genre, Platform } = require("./src/db.js");
+const { API_KEY } = process.env;
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: true }).then(async () => {
+  // add Genres to the database
+  const verifyGenres = await Genre.findAll();
+  if (!verifyGenres.length) {
+    const ApiGenres = await axios.get(
+      `https://api.rawg.io/api/genres?key=${API_KEY}`
+    );
+    const addGenres = await Genre.bulkCreate(
+      ApiGenres.data.results?.map((genre) => {
+        return {
+          id: genre.id,
+          name: genre.name,
+        };
+      })
+    );
+  }
+  // Add Platforms to the database
+  const verifyPlatforms = await Platform.findAll();
+  if (!verifyPlatforms.length) {
+    const ApiPlatforms = await axios.get(
+      `https://api.rawg.io/api/platforms?key=${API_KEY}`
+    );
+    const addPlatforms = await Platform.bulkCreate(
+      ApiPlatforms.data.results?.map((platform) => {
+        return {
+          id: platform.id,
+          name: platform.name,
+        };
+      })
+    );
+  }
+
   server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
