2765b34b9846760d68527bfe398aa3b9fc3a0973
Maximiliano Cidron <maximilianocidron@gmail.com>
2022-01-11 20:41:19 -0300

    Frontend first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
-import './App.css';
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import Landing from "./components/Landing/landing.jsx";
+import NavBar from "./components/NavBar/navbar.jsx";
+import Home from "./components/Home/home.jsx";
+import About from "./components/About/about.jsx";
+import Detail from "./components/Detail/detail.jsx";
+import Form from "./components/Form/form.jsx";
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Videogames</h1>
+      <Switch>
+        <Route exact path="/landing" component={Landing} />
+        <Route path="/">
+          <NavBar />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/videogame/:id" component={Detail} />
+          <Route exact path="/videogame" component={Form} />
+          <Route exact path="/about" component={About} />
+        </Route>
+      </Switch>
     </div>
   );
 }
