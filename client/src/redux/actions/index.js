f979ccb2160f4e8f51a7a0a898d27a30bbb3fb30
Maximiliano Cidron <maximilianocidron@gmail.com>
2022-01-12 19:50:57 -0300

    actions | reducer | store | first commit

---
+++ client/src/redux/actions/index.js
@@ -0,0 +1,125 @@
+import axios from "axios";
+
+export const GET_ALL_GAMES = "GET_ALL_GAMES";
+export const GET_GAMES_BY_NAME = "GET_GAMES_BY_NAME";
+export const GET_GAME_BY_ID = "GET_GAME_BY_ID";
+export const GET_GENRES = "GET_GENRES";
+export const GET_PLATFORMS = "GET_PLATFORMS";
+export const POST_GAME = "POST_GAME";
+export const FILTER_BY_GENRE = "FILTER_BY_GENRE";
+export const FILTER_BY_PLATFORM = "FILTER_BY_PLATFORM";
+export const FILTER_DB = "FILTER_DB";
+export const ORDER_BY_NAME = "ORDER_BY_NAME";
+export const ORDER_BY_RATING = "ORDER_BY_RATING";
+
+export const getAllGames = () => async (dispatch) => {
+  try {
+    const response = await axios.get("http://localhost:3001/videogames");
+    return dispatch({
+      type: GET_ALL_GAMES,
+      payload: response.data,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getGamesByName = (name) => async (dispatch) => {
+  try {
+    const response = await axios.get(
+      `http://localhost:3001/videogames?name=${name}`
+    );
+    return dispatch({
+      type: GET_GAMES_BY_NAME,
+      payload: response.data,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getGameById = (id) => async (dispatch) => {
+  try {
+    const response = await axios.get(`http://localhost:3001/videogame/${id}`);
+    return dispatch({
+      type: GET_GAME_BY_ID,
+      payload: response.data,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getGenres = () => async (dispatch) => {
+  try {
+    const response = await axios.get("http://localhost:3001/genres");
+    return dispatch({
+      type: GET_GENRES,
+      payload: response.data,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getPlatforms = () => async (dispatch) => {
+  try {
+    const response = await axios.get("http://localhost:3001/platforms");
+    return dispatch({
+      type: GET_PLATFORMS,
+      payload: response.data,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const postGame = (payload) => async (dispatch) => {
+  try {
+    const response = await axios.post(
+      "http://localhost:3001/videogame",
+      payload
+    );
+    return dispatch({
+      type: POST_GAME,
+      payload: response.data,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const filterByGenre = (payload) => {
+  return {
+    type: FILTER_BY_GENRE,
+    payload,
+  };
+};
+
+export const filterByPlatform = (payload) => {
+  return {
+    type: FILTER_BY_PLATFORM,
+    payload,
+  };
+};
+
+export const filterDB = (payload) => {
+  return {
+    type: FILTER_DB,
+    payload,
+  };
+};
+
+export const orderByName = (payload) => {
+  return {
+    type: ORDER_BY_NAME,
+    payload,
+  };
+};
+
+export const orderByRating = (payload) => {
+  return {
+    type: ORDER_BY_RATING,
+    payload,
+  };
+};
---
+++ client/src/redux/reducer/index.js
@@ -0,0 +1,135 @@
+import {
+  GET_ALL_GAMES,
+  GET_GAMES_BY_NAME,
+  GET_GAME_BY_ID,
+  GET_GENRES,
+  GET_PLATFORMS,
+  POST_GAME,
+  FILTER_BY_GENRE,
+  FILTER_BY_PLATFORM,
+  FILTER_DB,
+  ORDER_BY_NAME,
+  ORDER_BY_RATING,
+} from "../actions/index.js";
+
+const initialState = {
+  videogames: [],
+  filterVideogames: [],
+  genres: [],
+  platforms: [],
+  details: {},
+};
+
+function rootReducer(state = initialState, { type, payload }) {
+  switch (type) {
+    case GET_ALL_GAMES: {
+      return {
+        ...state,
+        videogames: payload,
+        filterVideogames: payload,
+      };
+    }
+    case GET_GAMES_BY_NAME: {
+      return {
+        ...state,
+        videogames: payload,
+      };
+    }
+    case GET_GAME_BY_ID: {
+      return {
+        ...state,
+        details: payload,
+      };
+    }
+    case GET_GENRES: {
+      return {
+        ...state,
+        genres: payload,
+      };
+    }
+    case GET_PLATFORMS: {
+      return {
+        ...state,
+        platforms: payload,
+      };
+    }
+    case POST_GAME: {
+      return {
+        ...state,
+        videogames: [...state.videogames, payload],
+      };
+    }
+    case FILTER_BY_GENRE: {
+      const filterVideogames = state.filterVideogames;
+      const genreFilter =
+        payload === "All"
+          ? filterVideogames
+          : filterVideogames.filter((videogame) => {
+              return videogame.genres?.includes(payload);
+            });
+      return {
+        ...state,
+        filterVideogames: genreFilter,
+      };
+    }
+    case FILTER_BY_PLATFORM: {
+      const filterVideogames = state.filterVideogames;
+      const platformFilter =
+        payload === "All"
+          ? filterVideogames
+          : filterVideogames.filter((videogame) => {
+              return videogame.platforms?.includes(payload);
+            });
+      return {
+        ...state,
+        filterVideogames: platformFilter,
+      };
+    }
+    case FILTER_DB: {
+      const filterVideogames = state.filterVideogames;
+      const filterDb =
+        payload === "Created"
+          ? filterVideogames.filter((videogame) => videogame.createdInDb)
+          : filterVideogames.filter((videogame) => !videogame.createdInDb);
+      return {
+        ...state,
+        videogames: payload === "All" ? filterVideogames : filterDb,
+      };
+    }
+    case ORDER_BY_NAME: {
+      const sortedByName =
+        payload === "A-Z"
+          ? [...state.videogames].sort((a, b) => {
+              if (a.name < b.name) return -1;
+              if (a.name > b.name) return 1;
+              return 0;
+            })
+          : [...state.videogames].sort((a, b) => {
+              if (a.name > b.name) return -1;
+              if (a.name < b.name) return 1;
+              return 0;
+            });
+      return {
+        ...state,
+        videogames: sortedByName,
+      };
+    }
+    case ORDER_BY_RATING: {
+      const sortedByRating =
+        payload === "High"
+          ? [...state.videogames].sort((a, b) => b.rating - a.rating)
+          : [...state.videogames].sort((a, b) => a.rating - b.rating);
+      return {
+        ...state,
+        videogames: sortedByRating,
+      };
+    }
+    default: {
+      return {
+        ...state,
+      };
+    }
+  }
+}
+
+export default rootReducer;
