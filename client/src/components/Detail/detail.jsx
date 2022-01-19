c913744b10a147a5457f21e5fc0f1091130a845e
Maximiliano Cidron <maximilianocidron@gmail.com>
2022-01-19 04:15:30 -0300

    DetailCss ready

--- client/src/components/Detail/detail.jsx
+++ client/src/components/Detail/detail.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from "react";
-import { useParams, useHistory } from "react-router-dom";
+import { useParams, useHistory, Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import {
   getGameById,
   deleteGame,
   getAllGames,
 } from "../../redux/actions/index.js";
-//import styles from "./detail.module.css";
+import styles from "./detail.module.css";
+import Loading from "../../assets/Loading.gif";
 
 export default function Detail() {
   const { id } = useParams();
@@ -27,42 +28,77 @@ export default function Detail() {
     history.push("/home");
   };
 
+  // function RatingStars(rating) {
+  //   const stars = {};
+  //   rating >= 1 && rating < 2
+  //     ? (stars.rating = `${rating} ★☆☆☆☆`)
+  //     : rating >= 2 && rating < 3
+  //     ? (stars.rating = `${rating} ★★☆☆☆`)
+  //     : rating >= 3 && rating < 4
+  //     ? (stars.rating = `${rating} ★★★☆☆`)
+  //     : rating >= 4 && rating < 5
+  //     ? (stars.rating = `${rating} ★★★★☆`)
+  //     : (stars.rating = `${rating} ★★★★★`);
+  //   return stars;
+  // }
+
   // let parser = new DOMParser();
   // let doc = parser.parseFromString(detail.description, "text/html");
   // let description = doc.body.innerText;
 
   return (
-    <div>
+    <div className={styles.loading_background}>
       {videogames[0] ? (
-        <div>
-          <h2>Name: {videogames[0].name} </h2>
-          <img
-            src={videogames[0].background_image}
-            alt={`${videogames[0].background_image}.jpg`}
-            width="600px"
-            height="400px"
-          />
-          <h3>Description: {videogames[0].description}</h3>
-          <h3>Released: {videogames[0].released}</h3>
-          <h3>Rating: {videogames[0].rating}</h3>
-          <h3>
-            Genres:{" "}
-            {videogames[0].genres.map((genre) => genre.name).join(" / ")}
-          </h3>
-          <h3>
-            Platforms:{" "}
-            {videogames[0].platforms
-              .map((platform) => platform.name)
-              .join(" / ")}
-          </h3>
-          {videogames[0].createdInDb === true ? (
-            <div>
-              <button onClick={(e) => handleDelete(e)}>DELETE GAME</button>
+        <div className={styles.background}>
+          <div className={styles.container}>
+            <div className={styles.name}> {videogames[0].name} </div>
+            <img
+              className={styles.image}
+              src={videogames[0].background_image}
+              alt={`${videogames[0].background_image}.jpg`}
+              width="950px"
+              height="600px"
+            />
+            <text className={styles.description}>
+              <div>Description</div>
+              {videogames[0].description}
+            </text>
+            <div className={styles.data}>
+              <div>Released</div> {videogames[0].released}
             </div>
-          ) : null}
+            <div className={styles.data}>
+              <div>Rating</div> {videogames[0].rating}
+            </div>
+            <div className={styles.data}>
+              <div>Genres</div>
+              {videogames[0].genres.map((genre) => genre.name).join(", ")}
+            </div>
+            <div className={styles.data}>
+              <div>Platforms</div>
+              {videogames[0].platforms
+                .map((platform) => platform.name)
+                .join(", ")}
+            </div>
+            {videogames[0].createdInDb === true ? (
+              <div>
+                <button
+                  className={styles.delete_button}
+                  onClick={(e) => handleDelete(e)}
+                >
+                  DELETE GAME
+                </button>
+              </div>
+            ) : (
+              <div className={styles.create_button}>
+                <Link to="/videogame">ADD YOUR OWN GAME</Link>
+              </div>
+            )}
+          </div>
         </div>
       ) : (
-        <h1>Loading...</h1>
+        <div>
+          <img className={styles.loading} src={Loading} alt="Loading" />
+        </div>
       )}
     </div>
   );
